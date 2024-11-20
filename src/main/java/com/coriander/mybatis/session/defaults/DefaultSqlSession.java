b04c06544edb4c9a158550ca2bf99ab8a2aa8465
xdybn <1187590793@qq.com>
2024-11-20 18:01:26 +0800

    Mapper XML的解析和注册使用

---
+++ src/main/java/com/coriander/mybatis/session/defaults/DefaultSqlSession.java
@@ -0,0 +1,44 @@
+package com.coriander.mybatis.session.defaults;
+
+import com.coriander.mybatis.binding.MapperRegistry;
+import com.coriander.mybatis.mapping.MappedStatement;
+import com.coriander.mybatis.session.Configuration;
+import com.coriander.mybatis.session.SqlSession;
+
+/**
+ * @Author YaoBoning
+ * @Date 2024/11/20 11:14
+ * @Description 默认SqlSession实现类
+ */
+public class DefaultSqlSession implements SqlSession {
+
+    private Configuration configuration;
+
+    public DefaultSqlSession(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public <T> T selectOne(String statement) {
+        return (T) ("你被代理了！" + statement);
+    }
+
+    @Override
+    public <T> T selectOne(String statement, Object parameter) {
+        MappedStatement mappedStatement = configuration.getMappedStatement(statement);
+        return (T) ("你被代理了！" + "\n方法：" + statement + "\n入参：" + parameter + "\n待执行SQL：" + mappedStatement.getSql());
+    }
+
+    @Override
+    public <T> T getMapper(Class<T> type) {
+        return configuration.getMapper(type, this);
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+}
+
+
